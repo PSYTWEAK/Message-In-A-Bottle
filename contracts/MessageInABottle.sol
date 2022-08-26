426dfc429303840374af38badc507297f9c23d73
PSYTWEAK <psy@promise.exchange>
2022-08-26 13:52:20 +0100

    contract

--- contracts/MessageInABottle.sol
+++ contracts/MessageInABottle.sol
@@ -1,34 +1,100 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-// Import this file to use console.log
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Library.sol";
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
+contract MessageInABottle is ERC721, Ownable {
 
-    event Withdrawal(uint amount, uint when);
+    using Library for uint8;
+    using ECDSA for bytes32;
 
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
+    uint public totalSupply;
+    mapping(uint256 => string) internal _message;
+
+    
+    constructor() ERC721("MessageInABottle", "MIAB") {
+    }
+
+
+    
+    function sendMessage(string memory message, address to) external payable {
+        
+        totalSupply++;
+
+        uint256 thisTokenId = totalSupply;
+  
+        _message[thisTokenId] = message;
+
+        _mint(to, thisTokenId);  
+
+    }
+
+
+   /**
+     * @dev Hash to SVG function
+     */
+    function SVG(uint tokenId)
+        public
+        view
+        returns (string memory)
+    {
+
+        string memory svgString = string(
+            abi.encodePacked(
+
+                "<svg viewBox='0 0 240 80' xmlns='http://www.w3.org/2000/svg'><style>.small { font: italic 13px sans-serif; }</style><text x='55' y='55' class='small'>",
+                _message[tokenId],
+                "</text></svg>")
         );
 
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+        return svgString;
     }
 
-    function withdraw() public {
-        // Uncomment this line to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+    /**
+     * @dev Hash to metadata function
+     */
+    function metadata()
+        public
+        view
+        returns (string memory)
+    {
+        string memory metadataString = string(abi.encodePacked('{"display_type": "Message", "trait_type": "Message", "value":',"message",'}'));
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
-
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
+        return string(abi.encodePacked("[", metadataString, "]"));
     }
+
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        return
+            string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    Library.encode(
+                        bytes(
+                            string(
+                                abi.encodePacked(
+                                    '{"name": "Message #',
+                                    Library.toString(_tokenId),
+                                    '", "description": " A message", "image": "data:image/svg+xml;base64,',
+                                    Library.encode(
+                                        bytes(SVG(_tokenId))
+                                    ),
+                                    '","attributes":',
+                                    metadata(),
+                                    "}"
+                                )
+                            )
+                        )
+                    )
+                )
+            );
+    }
+
 }
